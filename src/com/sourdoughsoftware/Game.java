9bf9e4080f67d72d2df239f9c3abf8bcb190b5ad
Tyrone Moore <mooret025@gmail.com>
2021-01-27 16:09:33 -0600

    created simple welcome screen class

--- src/com/sourdoughsoftware/Game.java
+++ src/com/sourdoughsoftware/Game.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Game {
     public static void main(String[] args) {
         InputParser prompt = new InputParser(new Scanner(System.in));
-        System.out.println("This is gonna be great.");
+        WelcomeScreen.getWelcomeMessage();
         String userEntry = prompt.prompt(">> ");
         System.out.println(userEntry);
     }
