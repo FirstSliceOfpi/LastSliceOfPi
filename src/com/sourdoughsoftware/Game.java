e04a2be907eb57fffd9ebc857e229b513fd0c8e8
sunflail <sunflail2001@yahoo.com>
2021-01-27 13:58:13 -0800

    Added basic Player class with constructor( name, location) , accessors, and toString() override

--- src/com/sourdoughsoftware/Game.java
+++ src/com/sourdoughsoftware/Game.java
@@ -7,8 +7,13 @@ import java.util.Scanner;
 public class Game {
     public static void main(String[] args) {
         InputParser prompt = new InputParser(new Scanner(System.in));
+
         System.out.println("This is gonna be great.");
-        String userEntry = prompt.prompt(">> ");
-        System.out.println(userEntry);
+        String userName = prompt.prompt("Enter your name adventurer\n>> ");
+//        System.out.println(userEntry);
+
+        Player player1 = new Player(userName,"Room 0");
+
+        System.out.println(player1.toString());
     }
 }
