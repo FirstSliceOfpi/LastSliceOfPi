80055577b48b3f3a346b92a6cdd3ee786a540e89
Tyrone Moore <mooret025@gmail.com>
2021-01-27 14:09:38 -0600

    Refactored name for game

--- src/com/sourdoughsoftware/Game.java
+++ src/com/sourdoughsoftware/Game.java
@@ -1,6 +1,6 @@
 package com.sourdoughsoftware;
 
-public class game {
+public class Game {
     public static void main(String[] args) {
         System.out.println("This is gonna be great.");
     }
