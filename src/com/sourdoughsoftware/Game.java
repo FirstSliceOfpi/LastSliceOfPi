d5da715c15cb674ae7d21ab65a68ab7b07e7b34b
sunflail <sunflail2001@yahoo.com>
2021-01-28 13:12:08 -0800

    Game reordering

--- src/com/sourdoughsoftware/Game.java
+++ src/com/sourdoughsoftware/Game.java
@@ -6,20 +6,23 @@ public class Game {
     public static void main(String[] args) {
         InputParser prompt = new InputParser(new Scanner(System.in));
         WelcomeScreen.getWelcomeMessage();
-        String userEntry = prompt.prompt(">> ");
-        System.out.println(userEntry);
-
+        Story.readFile();
         System.out.println("This is gonna be great.");
         String userName = prompt.prompt("Enter your name adventurer\n>> ");
-//        System.out.println(userEntry);
-
         Player player1 = new Player(userName,"Room 0");
-
         System.out.println(player1.toString());
-
         player1.addInventory("Knife");
         System.out.println(player1.toString());
+        String userEntry = prompt.prompt(">> ");
+//        System.out.println(userEntry);
 
-        Story.readFile();
+
+//        System.out.println(userEntry);
+
+
+
+
+
+
     }
 }
