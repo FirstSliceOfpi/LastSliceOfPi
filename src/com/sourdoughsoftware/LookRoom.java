436de2c66a3e8706ca8504c5665c0011765b60e5
GinVill <ginayvillegas@gmail.com>
2021-02-04 09:45:41 -0800

    Worked on roomLook feature for player - not resolved yet

--- src/com/sourdoughsoftware/LookRoom.java
+++ src/com/sourdoughsoftware/LookRoom.java
@@ -4,6 +4,14 @@ import java.util.List;
 
 public class LookRoom {
 
+    /**
+     * This method takes in the current room, player's response, room list and play
+     * @param roomID
+     * @param response
+     * @param roomList
+     * @param player1
+     * @return
+     */
     public static String roomLook(Integer roomID, String response, List<Room> roomList, Player player1) {
         String result = "";
         for (Room room : roomList) {
@@ -13,9 +21,16 @@ public class LookRoom {
                     result = "Nothing to see here.";
                     break;
                 }
+                player1.setPlayerRoomID(exit);
                 result = room.getShortDescription();
+                Room tempRoom = new Room();
+                tempRoom = room.setRoomID(exit);
+                System.out.println(tempRoom.getRoomID());
+
+                result = tempRoom.getShortDescription();
             }
         }
         return result;
     }
+
 }
