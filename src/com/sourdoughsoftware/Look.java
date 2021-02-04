4f59969b645d7ce50ba3ee57e03063ee78e73521
sunflail <sunflail2001@yahoo.com>
2021-02-03 17:02:59 -0800

    Many changes - see merge comment

---
+++ src/com/sourdoughsoftware/Look.java
@@ -0,0 +1,37 @@
+package com.sourdoughsoftware;
+
+import java.util.List;
+
+public class Look {
+
+    public static String roomLook(Integer roomID, String response, List<Room> roomList, Player player1) {
+        String result = "";
+        for (Room room : roomList) {
+            if (room.getRoomID().equals(roomID)) {
+                Integer exit = room.getExitByID(response);
+                if (exit == null) {
+                    result = "Nothing to see here.";
+                    break;
+                }
+                result = room.getShortDescription();
+            }
+        }
+        return result;
+    }
+
+    public static String itemLook(Integer playerRoomID, String response, List<Room> roomList, List<Item> itemList) {
+        String result = "What are you looking at?";
+        for (Item item : itemList) {
+            if (item.getName().equalsIgnoreCase(response)) {
+                for (Room room : roomList) {
+                    if (playerRoomID.equals(room.getRoomID()) && room.getRoomItems().contains(item)) {
+                        result = item.getDescription();
+                        break;
+                    }
+                }
+            }
+
+        }
+        return result;
+    }
+}
