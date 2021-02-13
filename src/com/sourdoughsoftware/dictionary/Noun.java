e606e8c9b0ad5b949811782e8e8e2500ac1f86e4
dustinmorris1 <dustinnuc@gmail.com>
2021-02-12 22:10:10 -0800

    removed unneeded functions and fields in Noun  AB#579

--- src/com/sourdoughsoftware/dictionary/Noun.java
+++ src/com/sourdoughsoftware/dictionary/Noun.java
@@ -11,23 +11,9 @@ public class Noun implements DictionaryEntry {
     private String name;
     private String description;
 
-
-    private boolean feelable = false;
-    private boolean eatable = false;
-    private boolean breakable = false;
-    private boolean openable = false;
-    private boolean closeable = false;
-    private boolean squishable = false;
-    private boolean bakeable = false;
-    private boolean rubable = false;
-    private boolean readable = false;
-    private boolean wearable = false;
     private boolean weildable = false;
-    private boolean drinkable = false;
     private boolean examinable = false;
-    private boolean dropable = false;
     private boolean grabable = false;
-    private boolean useable = false;
     private boolean mergeable = false;
     private boolean attackable = false;
     private boolean findable = false;
@@ -92,50 +78,6 @@ public class Noun implements DictionaryEntry {
         this.findable = findable;
     }
 
-    public boolean isFeelable() {
-        return feelable;
-    }
-
-    public void setFeelable(boolean feelable) {
-        this.feelable = feelable;
-    }
-
-    public boolean isEatable() {
-        return eatable;
-    }
-
-    public void setEatable(boolean eatable) {
-        this.eatable = eatable;
-    }
-
-    public boolean isBreakable() {
-        return breakable;
-    }
-
-    public void setBreakable(boolean breakable) {
-        this.breakable = breakable;
-    }
-
-    public boolean isOpenable() {
-        return openable;
-    }
-
-    public void setOpenable(boolean openable) {
-        this.openable = openable;
-    }
-
-    public boolean isCloseable() {
-        return closeable;
-    }
-
-    public void setCloseable(boolean closeable) {
-        this.closeable = closeable;
-    }
-
-    public boolean isSquishable() {
-        return squishable;
-    }
-
     public boolean isMergeable() {
         return mergeable;
     }
@@ -152,56 +94,12 @@ public class Noun implements DictionaryEntry {
         this.attackable = attackable;
     }
 
-    public void setSquishable(boolean squishable) {
-        this.squishable = squishable;
-    }
-
-    public boolean isBakeable() {
-        return bakeable;
-    }
-
-    public void setBakeable(boolean bakeable) {
-        this.bakeable = bakeable;
-    }
-
-    public boolean isRubable() {
-        return rubable;
-    }
-
-    public void setRubable(boolean rubable) {
-        this.rubable = rubable;
-    }
-
-    public boolean isReadable() {
-        return readable;
-    }
-
-    public void setReadable(boolean readable) {
-        this.readable = readable;
-    }
-
-    public boolean isWearable() {
-        return wearable;
-    }
-
-    public void setWearable(boolean wearable) {
-        this.wearable = wearable;
-    }
-
     public boolean isWeildable() {
         return weildable;
     }
 
     public void setWeildable(boolean weildable) {
         this.weildable = weildable;
-    }
-
-    public boolean isDrinkable() {
-        return drinkable;
-    }
-
-    public void setDrinkable(boolean drinkable) {
-        this.drinkable = drinkable;
     }
 
     public boolean isExaminable() {
@@ -212,28 +110,12 @@ public class Noun implements DictionaryEntry {
         this.examinable = examinable;
     }
 
-    public boolean isDropable() {
-        return dropable;
-    }
-
-    public void setDropable(boolean dropable) {
-        this.dropable = dropable;
-    }
-
     public boolean isGrabable() {
         return grabable;
     }
 
     public void setGrabable(boolean grabable) {
         this.grabable = grabable;
-    }
-
-    public boolean isUseable() {
-        return useable;
-    }
-
-    public void setUseable(boolean useable) {
-        this.useable = useable;
     }
 
 }
